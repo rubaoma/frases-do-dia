7165e7e31f81be28ce8beab3c63642be79a57c49
Rubens MA <rubao.ma@gmail.com>
2020-06-24 09:09:19 -0300

    adicionando função randomica de frases

--- app/src/main/java/com/rubdev/frasesdodia/MainActivity.java
+++ app/src/main/java/com/rubdev/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.rubdev.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view) {
+
+        String[] frases = {
+                "Frase 1",
+                "Frase 2",
+                "Frase 3",
+                "Frase 4"
+        };
+
+        TextView texto = findViewById(R.id.textResultado);
+        int numero = new Random().nextInt(4);
+        texto.setText( frases[numero]);
+
+    }
+
+
 }
